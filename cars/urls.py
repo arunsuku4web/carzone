d22d184c02815a74c6988f280484a3e8752d41a3
Arun Sukumar <arun.greenfrog@gmail.com>
2024-01-03 08:32:44 +0530

    Search functionalites and Admin custamization

--- cars/urls.py
+++ cars/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 urlpatterns = [
 
   path('',views.cars,name='cars'),
-
-
+  path('<int:id>',views.car_detail,name='car_detail'),
+  path('search',views.search,name='search'),
 ]
